d318a77f589ff732d99db732b63b7874c4963555
Sudip Das <sudip.dasaus@gmail.com>
2024-05-26 20:52:46 +1000

    Type of variables.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+
 }
 
 resource "azurerm_resource_group" "devrg" {
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,28 @@
 variable "resource_group" {}
 variable "SGport" {}
 variable "location" {}
+
+variable "app prot" {
+    default = "8080"
+}
+
+#List data type
+variable "my-list" {
+    type = list
+    default = ["mumbai", "kolkata", "Delhi"]
+}
+
+variable "my-list" {
+    type = list(number)
+    default = ["1", "2", "3"]
+}
+
+#MAP data type
+variable "virtual_machine_tag" {
+  type = map
+  default = {
+    app = "webmethod"
+    environment = "development"
+    dept = "Infra"
+  }
+}
